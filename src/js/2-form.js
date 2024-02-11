9339b5d99020dc4119e85d48f2d91816d103f374
IrinaPapulova <142151842+IrinaPapulova@users.noreply.github.com>
2024-02-11 13:11:29 +0200

    change fedback-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,38 +1,56 @@
 const form = document.querySelector('.feedback-form');
 const LS_KEY = 'feedback-form-state';
 
-let emailForm = form.elements.email.value.trim();
-let messageForm = form.elements.message.value.trim();
 let formValue = {};
 
-form.addEventListener('input', evt => {
-  emailForm = form.elements.email.value.trim();
-  messageForm = form.elements.message.value.trim();
+const emailForm = form.elements.email;
+const messageForm = form.elements.message;
 
-  if (emailForm && messageForm) {
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if(emailForm.value === "" || messageForm.value === "") {
+    alert("All form fields must be filled in");
+  } else {
     formValue = {
-      email: emailForm,
-      message: messageForm,
-    };
-
+      email: emailForm.value.trim(),
+      message: messageForm.value.trim(),
+    }
     localStorage.setItem(LS_KEY, JSON.stringify(formValue));
+    console.log(formValue);
+    localStorage.removeItem(LS_KEY);
+    event.currentTarget.reset();
   }
-});
+};
 
-let savedFormValue = localStorage.getItem(LS_KEY);
-if (savedFormValue) {
-  formValue = JSON.parse(savedFormValue);
-} else {
-  form.elements.email.value = "";
-form.elements.message.value = "";
+form.addEventListener('input', onFormInput);
+
+function onFormInput(event) {
+  formValue = {
+    email: emailForm.value.trim(),
+    message: messageForm.value.trim(),
+  }
+
+  localStorage.setItem(LS_KEY, JSON.stringify(formValue));
 }
 
-form.elements.email.value = formValue.email;
-form.elements.message.value = formValue.message;
+function fillFields() { 
+  const savedFormValue = localStorage.getItem(LS_KEY);
+  if (savedFormValue) {
+    const parsedFormValue = JSON.parse(savedFormValue);
+    form.elements.email.value = parsedFormValue.email;
+    form.elements.message.value = parsedFormValue.message;
+  } else {
+    form.elements.email.value = '';
+    form.elements.message.value = '';
+  }
+}
 
-form.addEventListener('submit', evt => {
-  evt.preventDefault();
-  console.log(formValue);
-  localStorage.removeItem(LS_KEY);
-  form.reset();
-});
+fillFields();
+
+
+
+
+
